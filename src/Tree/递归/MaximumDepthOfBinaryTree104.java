84760f4df9b318c16cdb08d4f49fea26c6f2d0d6
yangling <418791825@qq.com>
2019-07-12 13:05:35 +0800

    剑指offer

---
+++ src/Tree/递归/MaximumDepthOfBinaryTree104.java
@@ -0,0 +1,17 @@
+package Tree.递归;
+
+import Tree.TreeNode;
+
+/**
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/description/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
+ */
+public class MaximumDepthOfBinaryTree104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int height = 1;
+        height = height + Math.max(maxDepth(root.left), maxDepth(root.right));
+        return height;
+    }
+}
